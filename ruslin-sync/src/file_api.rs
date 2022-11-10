4368ae28c8cd420f7ab4ad62b2baeb670fce670b
DianQK <dianqk@dianqk.net>
2022-11-10 22:08:21 +0800

    feat: added file_api_driver_local and file_api_driver_memory

--- ruslin-sync/src/file_api.rs
+++ ruslin-sync/src/file_api.rs
@@ -1,5 +1,10 @@
 mod file_api_driver;
+mod file_api_driver_local;
+mod file_api_driver_memory;
+
 pub use file_api_driver::FileApiDriver;
+pub use file_api_driver_local::FileApiDriverLocal;
+pub use file_api_driver_memory::FileApiDriverMemory;
 
 pub struct FileApi {}
 
---
+++ ruslin-sync/src/file_api/file_api_driver_local.rs
@@ -0,0 +1,63 @@
+use super::FileApiDriver;
+
+pub struct FileApiDriverLocal {}
+
+impl FileApiDriver for FileApiDriverLocal {
+    fn supports_multi_put(&self) -> bool {
+        false
+    }
+
+    fn supports_accurate_timestamp(&self) -> bool {
+        false
+    }
+
+    fn supports_locks(&self) -> bool {
+        false
+    }
+
+    fn request_repeat_count(&self) -> u32 {
+        todo!()
+    }
+
+    fn stat(path: &str) -> crate::Result<super::file_api_driver::Stat> {
+        todo!()
+    }
+
+    fn list(
+        path: &str,
+        options: &super::file_api_driver::Options,
+    ) -> crate::Result<super::file_api_driver::StatList> {
+        todo!()
+    }
+
+    fn get(path: &str, options: &super::file_api_driver::Options) -> crate::Result<Option<String>> {
+        todo!()
+    }
+
+    fn mkdir(path: &str) -> crate::Result<()> {
+        todo!()
+    }
+
+    fn put(path: &str, options: &super::file_api_driver::Options) -> crate::Result<()> {
+        todo!()
+    }
+
+    fn multi_put(
+        items: &[super::file_api_driver::MultiPutItem],
+        options: &super::file_api_driver::Options,
+    ) -> crate::Result<()> {
+        todo!()
+    }
+
+    fn delete(path: &str) -> crate::Result<()> {
+        todo!()
+    }
+
+    fn r#move(old_path: &str, new_path: &str) -> crate::Result<()> {
+        todo!()
+    }
+
+    fn clear_root(base_dir: &str) -> crate::Result<()> {
+        todo!()
+    }
+}
