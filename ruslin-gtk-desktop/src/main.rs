b8474cfed56ee9b2f6768224472c45c2d5a20272
DianQK <dianqk@dianqk.net>
2022-11-12 20:33:48 +0800

    [gtk-desktop] listings/hello_world/1/main.rs

--- ruslin-gtk-desktop/src/main.rs
+++ ruslin-gtk-desktop/src/main.rs
@@ -1,3 +1,12 @@
+use gtk::prelude::*;
+use gtk::Application;
+
+const APP_ID: &str = "org.dianqk.ruslin-desktop.gtk";
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Run the application
+    app.run();
 }
