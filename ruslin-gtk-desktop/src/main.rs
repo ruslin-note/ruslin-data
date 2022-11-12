fe764c105f8d622009c214ccc4a36aeb0d38fe6f
DianQK <dianqk@dianqk.net>
2022-11-12 20:40:34 +0800

    [gtk-desktop] listings/hello_world/2/main.rs

--- ruslin-gtk-desktop/src/main.rs
+++ ruslin-gtk-desktop/src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::Application;
+use gtk::{Application, ApplicationWindow};
 
 const APP_ID: &str = "org.dianqk.ruslin-desktop.gtk";
 
@@ -7,6 +7,20 @@ fn main() {
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
     // Run the application
     app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Ruslin")
+        .build();
+
+    // Present window
+    window.present();
 }
